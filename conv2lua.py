6971ad215bd5d5dd6c633098d0b607f0708fe144
gaccob <weiye.zhang@qq.com>
2014-09-27 19:16:01 +0800

    update convert script and add samples

--- conv2lua.py
+++ conv2lua.py
@@ -1,41 +1,88 @@
 import xdrlib ,sys
 import xlrd
+import getopt
 
-def convert(xls, sheet, dest):
+##################################################################
+
+def convert_sheet(sheet, output_path):
+    nrows = sheet.nrows
+    ncols = sheet.ncols
+
+    keys = {}
+    for i in range(0, ncols):
+        keys[i] = sheet.cell(0, i).value
+
+    output = []
+    output.append("Config = {")
+    for i in range(1, nrows):
+        output.append("    {")
+        for j in keys:
+            val = sheet.cell(i, j).value
+            if val == '' or val == u'':
+                continue
+            line = "        %s = %s" % (keys[j], val)
+            line = ((j == ncols - 1) and line or ("%s, " % line)).encode("utf-8")
+            output.append(line)
+        output.append((i == nrows - 1) and "    }" or "    },")
+    output.append("}")
+
+    name = '%s/%s.lua' % (output_path, sheet.name)
+    try:
+        fd = open(name, 'w')
+    except Exception, e:
+        print str(e)
+        return
+
+    output = [line + '\n' for line in output]
+    fd.writelines(output)
+    fd.close()
+    print 'convert %s success' % name
+
+##################################################################
+
+def convert(xls, output_path):
     try:
         book = xlrd.open_workbook(xls)
     except Exception, e:
         print str(e)
         return
 
-    table = book.sheet_by_name(sheet)
-    nrows = table.nrows
-    ncols = table.ncols
+    # get sheet one by one
+    for sheet in book.sheets():
+        convert_sheet(sheet, output_path)
 
-    keys = {}
-    for i in range(0, ncols):
-        keys[i] = table.cell(0, i).value
+##################################################################
 
-    output = []
-    output.append("data = {")
-    for i in range(1, nrows):
-        output.append("    {")
-        for j in keys:
-            line = "        %s = %s" % (keys[j], table.cell(i, j).value)
-            line = ((j == ncols - 1) and line or ("%s, " % line)).encode("utf-8")
-            output.append(line)
-        output.append((i == nrows - 1) and "    }" or "    },")
-    output.append("}")
+def usage():
+    print '''
+    conv2lua.py usage:
+        -f, --excel_file    <excel file name>               [required]
+        -O, --output_path   <convert result's path>         [optional]
+    '''
+    quit()
 
-    try:
-        fd = open(dest, "w")
-    except Exception, e:
-        print str(e)
-        return
-    output = [line + '\n' for line in output]
-    fd.writelines(output)
-    fd.close()
+##################################################################
 
 if __name__=="__main__":
-    convert("./sample/prop.xls", "prop", "./sample/prop.lua")
+
+    sopt = 'f:O:'
+    lopt = ['excel_file=', 'output_path=']
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], sopt, lopt)
+    except Exception, e:
+        print str(e) 
+        usage()
+
+    excel = ''
+    output_path = '.'
+    for opt, val in opts:
+        if opt in ('-f', '--excel_file'):
+            excel = val
+        elif opt in ('-O', '--output_path'):
+            output_path = val
+
+    if excel == '':
+        usage()
+
+    convert(excel, output_path)
 
--- conv2pb.py
+++ conv2pb.py
@@ -3,6 +3,7 @@
 import xlrd
 import sys
 import os
+import getopt
 
 TMAP = {}
 TMAP[1] = int
@@ -19,14 +20,34 @@ LMAP[1] = 'optional'
 LMAP[2] = 'required'
 LMAP[3] = 'repeated'
 
+MOD = []
+
+PROTOC = 'protoc'
+TMP_FILE = []
+
+######################################################################
+
+def load_enum(module, enum_key):
+    if module.__dict__.has_key(enum_key):
+        return eval('module.%s' % val)
+    else:
+        for mod in MOD:
+            if mod.__dict__.has_key(enum_key):
+                return eval('mod.%s' % enum_key)
+    print 'cant find enum define [%s]' % enum_key
+    quit()
+
 ######################################################################
 
 def load_data(module, prototype, msg, key, val):
+    # if not defined in protocol, ignore
+    if key not in prototype.fields_by_name:
+        return
     field = prototype.fields_by_name[key]
 
     # value or enumerate
     if field.enum_type != None:
-        value = eval('module.%s' % val)
+        value = load_enum(module, val)
     else:
         if not field.cpp_type in TMAP:
             print "protobuf field[%s] invalid cpp type" % key
@@ -39,20 +60,43 @@ def load_data(module, prototype, msg, key, val):
         obj = getattr(msg, key)
         obj.append(value)
     else:
+        if type(value) == type(''):
+            value = unicode(value, 'utf8')
         msg.__setattr__(key, value)
 
 ######################################################################
 
-def convert_sheet(sheet, module, output):
+def convert_sheet_print(file_name, content):
+    try:
+        fd = open(file_name, 'w')
+    except:
+        print '%s open fail' % file_name
+        quit()
+    fd.write(content)
+    fd.close()
+
+######################################################################
+
+def convert_sheet_check(message, file_name):
+    try:
+        fd = open(file_name, 'rb')
+        message.ParseFromString(fd.read())
+    except:
+        print 'open check file[%s] fail' % file_name
+        quit()
+
+######################################################################
+
+def convert_sheet(sheet, module, output_path):
     config_name = sheet.name
     if not module.__dict__.has_key(config_name):
-        print ('protobuf define[%s] not found' % config_name)
-        quit()
+        print ('sheett [%s] not found in protobuf' % config_name)
+        return
 
     # protobuf data structure
     table = module.__dict__[config_name]()
-    
-    # proto type 
+
+    # proto type
     prototype = module.DESCRIPTOR.message_types_by_name[config_name]
     assert len(prototype.fields) > 0
     row_name = prototype.fields[0].name
@@ -72,70 +116,119 @@ def convert_sheet(sheet, module, output):
                 msg = rows.add()
             load_data(module, data_prototype, msg, key, val)
 
-    # output bin
-    output_file = '%s/%s.bin' % (output, config_name)
-    try:
-        fd = open(output_file, 'wb+')
-    except Except, e:
-        print '%s open fail: %s' % (output_file, str(e))
-        quit()
-    fd.write(table.SerializePartialToString())
+    # output bin file
+    bin_file = '%s/%s.bin' % (output_path, config_name)
+    convert_sheet_print(bin_file, table.SerializePartialToString())
+    convert_sheet_check(table, bin_file)
+    print 'convert %s success\n' % bin_file
 
-    # check
-    try:
-        check(table, output_file)
-    except Exception, e:
-        print '%s check fail: %s' % (output_file, str(e))
-    print 'convert %s success' % output_file
+    # output log file
+    log_file = '%s/%s.log' % (output_path, config_name)
+    from google.protobuf import text_format as mod
+    convert_sheet_print(log_file, mod.MessageToString(table))
 
 ######################################################################
 
-def convert(excel, proto, output):
+def load_import(import_path, proto):
+
+    imports = []
+    for f in os.listdir(import_path):
+        if f == proto:
+            continue
+        import_proto = os.path.splitext(f)[0]
+        suffix = os.path.splitext(f)[1]
+        if suffix == '.proto':
+            cmd = '%s --error_format=msvs -I%s --python_out=. %s/%s' % (PROTOC, import_path, import_path, f)
+            os.system(cmd)
+            imports.append(import_proto)
+
+    for p in imports:
+        mod = __import__('%s_pb2' % p)
+        TMP_FILE.append(p)
+        MOD.append(mod)
+        print 'mod[%s] loaded' % p
+
+######################################################################
+
+def convert(excel, proto, output_path, import_path):
+
+    proto_file = os.path.basename(proto)
+    proto_path = os.path.dirname(proto)
+    proto_name =  os.path.splitext(proto_file)[0]
+    proto_bin_name = '%s.pb' % proto_name
+
+    import_cmd = ''
+    if import_path != '':
+        import_cmd = '%s -I%s' % (import_cmd, import_path)
+    if proto_path != '':
+        import_cmd = '%s -I%s' % (import_cmd, proto_path)
 
     # generate python-protobuf-interface
-    os.system('protoc --error_format=msvs --python_out=%s %s' % (output, proto))
+    cmd = '%s --error_format=msvs %s --python_out=. %s ' % (PROTOC, import_cmd, proto)
+    os.system(cmd)
 
     # import protocol
-    proto_file = os.path.basename(proto)
-    proto_name =  os.path.splitext(proto_file)[0]
-    sys.path.append(output)
     module = __import__('%s_pb2' % proto_name)
+    TMP_FILE.append(proto_name)
 
     # open excel
     try:
         book = xlrd.open_workbook(excel)
-    except Exception, e:
-        print str(e)
+    except:
+        print 'open xls[%s] fail' % excel
         quit()
 
     # get sheet one by one
     for sheet in book.sheets():
-        convert_sheet(sheet, module, output)
+        convert_sheet(sheet, module, output_path)
+
+    # delete temporary file
+    for f in TMP_FILE:
+        os.system('rm ./%s_pb2.py*' % f)
 
 ######################################################################
 
-def check(message, file_name):
-    try:
-        fd = open(file_name, 'rb')
-        message.ParseFromString(fd.read())
-    except Exception, e:
-        print str(e)
-        quit()
+def usage():
+    print '''
+    conv2pb.py usage:
+        -f, --excel_file    <excel file name>               [required]
+        -p, --proto         <protobuf decript protocol>     [required]
+        -I, --import_path   <protobuf import path>          [optional]
+        -O, --output_path   <convert result's path>         [optional]
+    '''
+    quit()
 
 ######################################################################
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print '''
-            Usage:
-                converter2.py <excel> <protocol> [store path, default .]
-            Example:
-                converter2.py attr.xlsx attr.proto ../output
-        '''
-        quit()
+    sopt = 'f:p:I:O:'
+    lopt = ['excel_file=', 'proto=', 'import_path=', 'output_path=']
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], sopt, lopt)
+    except:
+        usage()
 
-    excel = sys.argv[1]
-    proto = sys.argv[2]
-    output = len(sys.argv) > 3 and sys.argv[3] or '.'
-    convert(excel, proto, output)
+    excel = ''
+    proto = ''
+    import_path = ''
+    output_path = '.'
+    for opt, val in opts:
+        if opt in ('-f', '--excel_file'):
+            excel = val
+        elif opt in ('-p', '--proto'):
+            proto = val
+        elif opt in ('-I', '--import_path'):
+            import_path = val
+        elif opt in ('-O', '--output_path'):
+            output_path = val
+
+    if excel == '' or proto == '':
+        usage()
+
+    sys.path.append('.')
+
+    if import_path != '':
+        load_import(import_path, proto)
+
+    convert(excel, proto, output_path, import_path)
 
